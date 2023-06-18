87f9510735cb7062b718341f2d4212cd477d1d1a
Lisa Walters <lisacampiwalters@gmail.com>
2023-06-17 20:46:50 -0600

    completed the 2nd exercise

--- app.js
+++ app.js
@@ -21,16 +21,27 @@ console.log("EXERCISE 1:\n==========\n");
 
 
 function printOdds(count){
-    if(count < 0){
-        count *= -1; 
+    if(count == 0){
+        console.log("The number should not be 0.");
     }
-    for(let i = 1; i <= count; i++){
-        if(i % 2 == 1){
-        console.log(i);
+
+    if(count < 0){
+        for(let i = -1; i >= count; i--){
+            if(i % -2 == -1){
+                console.log(i);
+            }
+        }
+    }
+
+    if (count > 0) {
+        for(let i = 1; i <= count; i++){
+            if(i % 2 == 1){
+            console.log(i);
+            }
         }
     }
 }
-printOdds(-103);
+printOdds(0);
 
 // Exercise 2 Section
 // console.log("EXERCISE 2:\n==========\n");
@@ -49,3 +60,22 @@ printOdds(-103);
 // an if/else statement that determines if the age value is below 16,
 // and then console.log() the correct message to the console.
 // BONUS: Remember that parameters are optional, and no userName or age value could be passed in. Correctly account for no parameter being passed in.
+
+function checkAge (age, userName = "Person"){
+    let aboveSixteen = `Congrats ${userName}, you can drive!`;
+    let belowSixteen = `Sorry ${userName}, but you need to wait until you're 16.`;
+    let undefinedAge = `Sorry, but I can't tell how old you are!`;
+    if(!Number.isInteger(age)){
+        console.log(undefinedAge);
+    }
+    else if (age >= 16){
+        console.log(aboveSixteen);
+    } else{
+        console.log(belowSixteen);
+    }
+}
+
+checkAge(9000, "Fred");
+
+// Exercise 3 Section
+// console.log("EXERCISE 3:\n==========\n");
